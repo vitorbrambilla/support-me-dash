c4dfa329c84035d8f1c9719673320d5713033a36
Vitor <vitor.reis@crmservices.com.br>
2024-03-15 18:23:42 -0300

    feat: initial navbar

---
+++ app/dashboard/components/menu-item.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type Props = {
+  children: React.ReactNode;
+  href: string;
+};
+
+export default function MenuItem({ children, href }: Props) {
+  const pathName = usePathname();
+  const isActive = pathName === href;
+
+  return (
+    <li>
+      <Link
+        className={cn(
+          "block p-2 hover:bg-white dark:hover:bg-zinc-700 rounded-md text-muted-foreground hover:text-foreground",
+          isActive &&
+            "bg-primary hover:bg-primary dark:hover:bg-primary hover:text-primary-oreground text-primary-foreground"
+        )}
+        href={href}
+      >
+        {children}
+      </Link>
+    </li>
+  );
+}
