96a6d3c0f449cd3463111706cc3e18f6c5b43858
Vitor <vitor.reis@crmservices.com.br>
2024-03-18 14:37:11 -0300

    feat: employees stats

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,20 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import EmployeesStats from "./components/employees/employees-stats";
+
 export default function DashboardPage() {
-  return <div>Dashboard</div>;
+  return (
+    <Tabs defaultValue="employees">
+      <TabsList className="mb-4">
+        <TabsTrigger value="employees">Employees stats</TabsTrigger>
+
+        <TabsTrigger value="teams">Teams stats</TabsTrigger>
+      </TabsList>
+
+      <TabsContent value="employees">
+        <EmployeesStats />
+      </TabsContent>
+
+      <TabsContent value="teams">teams stats</TabsContent>
+    </Tabs>
+  );
 }
