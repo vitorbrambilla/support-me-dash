ebdba79deab2ec32746756d1a50268e1dfc0ad54
Vitor <vitor.reis@crmservices.com.br>
2024-03-19 18:29:38 -0300

    feat: employees data table

---
+++ app/dashboard/employees/columns.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { ColumnDef } from "@tanstack/react-table";
+import Image from "next/image";
+
+export type Employee = {
+  avatar?: string;
+  id: number;
+  firstName: string;
+  lastName: string;
+  teamName: string;
+  isTeamLeader: boolean;
+};
+
+export const columns: ColumnDef<Employee>[] = [
+  {
+    accessorKey: "avatar",
+    header: "",
+    cell: ({ row }) => {
+      const avatar: string = row.getValue("avatar");
+      const firstName: string = row.getValue("firstName");
+      const lastName: string = row.getValue("lastName");
+
+      return (
+        <Avatar>
+          {!!avatar && (
+            <Image
+              height={40}
+              width={40}
+              src={avatar}
+              alt={`${firstName} ${lastName} avatar`}
+            />
+          )}
+
+          <AvatarFallback className="uppercase">
+            {firstName[0]}
+            {lastName[0]}
+          </AvatarFallback>
+        </Avatar>
+      );
+    },
+  },
+  {
+    accessorKey: "firstName",
+    header: "First name",
+  },
+  {
+    accessorKey: "lastName",
+    header: "Last name",
+  },
+  {
+    accessorKey: "teamName",
+    header: "Team",
+  },
+  {
+    accessorKey: "isTeamLeader",
+    header: "",
+    cell: ({ row }) => {
+      const isTeamLeader: boolean = row.getValue("isTeamLeader");
+
+      return isTeamLeader ? <Badge variant="success">Team leader</Badge> : null;
+    },
+  },
+];
